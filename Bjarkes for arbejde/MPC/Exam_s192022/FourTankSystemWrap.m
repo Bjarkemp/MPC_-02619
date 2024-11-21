92ef8520eb9b9ba3a2359ed8149ac432c7316bd3
Bjarkemp <Bjarkemp@hotmail.com>
2024-11-21 11:37:05 +0100

    slet
    har slettet

--- Bjarkes for arbejde/MPC/Exam_s192022/FourTankSystemWrap.m
+++
@@ -1,2 +0,0 @@
-function xdot = FourTankSystemWrap(x,u,d,p)
-xdot = QuadrupleTankProcess(0,x,u,d,p);
--- Bjarkes for arbejde/MPC/Exam_s192022/PControl.m
+++
@@ -1,6 +0,0 @@
-% Define Proportional Controller
-function u = PControl(r, y, us, Kc, umin, umax)
-    e = r - y;
-    v = us + Kc * e;
-    u = max(umin, min(umax, v));
-end
