dbea951c4432410661efba2320c74363707191c2
Bjarkemp <Bjarkemp@hotmail.com>
2024-11-21 11:51:13 +0100

    test

--- MPC-exam 2024/mass_to_height.m
+++ MPC-exam 2024/mass_to_height.m
@@ -4,3 +4,5 @@ A=p(1:end-1);                   %[cm2] Cross sectional area in all tanks
 rho=p(end);                  % [g/cm^3] Density of water
 
 h= x./ (rho * A');          % converting mass in [g] to height in [cm]
+
+test
